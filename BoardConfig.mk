493f564e3d51c5735fcc7226d87fb5a6d7b7f4e4
Nolen Johnson <johnsonnolen@gmail.com>
2020-07-13 18:35:22 -0400

    surya: Don't declare BOARD_VENDOR
    * Only matters when it differs from the device OEM.
    
    Change-Id: Id9af8263cb42dc7a0e3a9ce8677385e224910302
    Signed-off-by: HinohArata <mmgcntk@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -7,8 +7,6 @@
 BUILD_BROKEN_DUP_RULES := true
 BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES := true
 
-BOARD_VENDOR := xiaomi
-
 DEVICE_PATH := device/xiaomi/surya
 
 # Inherit from proprietary files
