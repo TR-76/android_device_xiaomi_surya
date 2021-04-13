61743be28be632064edf82cc27ad94a249224a1d
TR-76 <tr-76@gmx.at>
2021-04-13 16:36:34 +0200

    Add MG-APKS
    Signed-off-by: TR-76 <tr-76@gmx.at>

--- lineage_surya.mk
+++ lineage_surya.mk
@@ -12,6 +12,12 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit some common ArrowOS stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
+# MG-APKS
+$(call inherit-product-if-exists, vendor/mg-apks/config.mk)
+TARGET_EXCLUDES_AUDIOFX := true
+TARGET_EXCLUDES_PREBUILT_ETAR_CALENDAR := true
+TARGET_EXCLUDES_PREBUILT_SIMPLE_KEYBOARD := true
+
 TARGET_SCREEN_HEIGHT := 2400
 TARGET_SCREEN_WIDTH := 1080
 
