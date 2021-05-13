44b7bd5e83d3e28064b9eb8f469d99304377850f
TR-76 <tr-76@gmx.at>
2021-05-13 10:06:53 +0200

    Add MG-APKS
    Signed-off-by: TR-76 <tr-76@gmx.at>

--- lineage_surya.mk
+++ lineage_surya.mk
@@ -14,6 +14,9 @@ $(call inherit-product, device/xiaomi/surya/device.mk)
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
+# MG-APKS
+$(call inherit-product-if-exists, vendor/mg-apks/config.mk)
+
 PRODUCT_NAME := lineage_surya
 PRODUCT_DEVICE := surya
 PRODUCT_BRAND := POCO
