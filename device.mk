313130c9e3642d74e0a4a845cb1840326ca5e50d
TR-76 <tr-76@gmx.at>
2024-02-26 16:52:49 +0100

    Build fingerprint extension interfaces from source
    Signed-off-by: TR-76 <tr-76@gmx.at>

--- device.mk
+++ device.mk
@@ -150,6 +150,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
 
+PRODUCT_PACKAGES += \
+    com.fingerprints.extension@1.0.vendor \
+    vendor.goodix.hardware.fingerprintextension@1.0.vendor
+
 # FM
 PRODUCT_PACKAGES += \
     FM2 \
