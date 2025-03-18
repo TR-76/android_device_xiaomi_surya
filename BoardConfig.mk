762183f83660f5aa4a06a2c73ca183e79a2492be
Yumi Yukimura <me.cafebabe@gmail.com>
2025-03-18 18:05:43 +0800

    surya: Drop `TARGET_RECOVERY_DEVICE_MODULES`
    * It's deprecated on 15 QPR2
    * Building a library as module doesn't get it executed? and libinit
      still does its job in recovery mode without migrating this
    
    Change-Id: I7be000356fe7d7bd099615d334892d1686bece0e
    Signed-off-by: HinohArata <mmgcntk@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -68,7 +68,6 @@ ODM_MANIFEST_FILES += $(DEVICE_PATH)/configs/hidl/manifest-qva.xml
 
 # Init
 TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):libinit_surya
-TARGET_RECOVERY_DEVICE_MODULES := libinit_surya
 
 # Kernel
 BOARD_KERNEL_BASE := 0x00000000
