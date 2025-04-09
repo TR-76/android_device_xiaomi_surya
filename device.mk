47177e89e53dbfd944771d2acac91de2eb62fa71
Yumi Yukimura <me.cafebabe@gmail.com>
2025-04-09 23:21:25 +0800

    surya: Switch to `$(TARGET_COPY_OUT_RECOVERY)`
    Change-Id: I2a5b7a1a7ea431ba3a0f12bef5af04c5c465bc6f
    Signed-off-by: HinohArata <mmgcntk@gmail.com>

--- device.mk
+++ device.mk
@@ -330,7 +330,7 @@ PRODUCT_PACKAGES += \
     fastbootd
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/rootdir/etc/init.recovery.qcom.rc:recovery/root/init.recovery.qcom.rc
+    $(LOCAL_PATH)/rootdir/etc/init.recovery.qcom.rc:$(TARGET_COPY_OUT_RECOVERY)/root/init.recovery.qcom.rc
 
 # Sensors
 PRODUCT_PACKAGES += \
