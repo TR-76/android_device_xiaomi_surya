4d2f02ece60d0271396b4f2d81404bfcee4371d3
Bruno Martins <bgcngm@gmail.com>
2025-04-29 23:21:39 +0100

    surya: Set vendor init lib via soong config
    Change-Id: I8f7e6b987bee58dbf09ca9ad7cfd5a7e99e19c3b
    Signed-off-by: HinohArata <mmgcntk@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -66,9 +66,6 @@ DEVICE_MANIFEST_FILE += hardware/qcom-caf/sm8150/media/conf_files/sm6150/c2_mani
 DEVICE_MATRIX_FILE := hardware/qcom-caf/common/compatibility_matrix.xml
 ODM_MANIFEST_FILES += $(DEVICE_PATH)/configs/hidl/manifest-qva.xml
 
-# Init
-TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):libinit_surya
-
 # Kernel
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_IMAGE_NAME := Image.gz
--- device.mk
+++ device.mk
@@ -218,6 +218,8 @@ PRODUCT_PACKAGES += \
     init.qcom.usb.rc \
     init.qcom.usb.sh
 
+$(call soong_config_set,libinit,vendor_init_lib,//$(LOCAL_PATH):libinit_surya)
+
 # IRSC
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/security/sec_config:$(TARGET_COPY_OUT_VENDOR)/etc/sec_config
