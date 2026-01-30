fcb0f445fa436fafc18d83cf3ea7287e273e8e5c
TR-76 <tr-76@gmx.net>
2026-01-30 11:18:48 +0100

    Revert "surya: Remove unneeded packages during build"
    This reverts commit 26357536a939fbe6afbc98f8ab8fdb8569db541d.

--- device.mk
+++ device.mk
@@ -352,10 +352,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/etc/init.recovery.qcom.rc:$(TARGET_COPY_OUT_RECOVERY)/root/init.recovery.qcom.rc
 
-# Remove Packages
-PRODUCT_PACKAGES += \
-    RemovePackages
-
 # Sensors
 PRODUCT_PACKAGES += \
     android.hardware.sensors-service.multihal
